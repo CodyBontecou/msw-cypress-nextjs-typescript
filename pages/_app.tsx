054437244313e929e0359c01b0df1e383b76dc18
Cody Bontecou <bontecouc@gmail.com>
2021-08-28 21:32:14 -0500

    nprogress for simple loading graphics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,37 +4,23 @@ import { FC } from 'react'
 require('../mocks')
 import Router from 'next/router'
 import React from 'react'
+import NProgress from 'nprogress'
 
 const Noop: FC = ({ children }) => <> </>
+
+Router.events.on('routeChangeStart', url => {
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({
   Component,
   pageProps,
 }: AppProps & { Component: { Layout: FC } }) {
-  const [loading, setLoading] = React.useState(false)
-  React.useEffect(() => {
-    const start = () => {
-      console.log('start')
-      setLoading(true)
-    }
-    const end = () => {
-      console.log('finished')
-      setLoading(false)
-    }
-    Router.events.on('routeChangeStart', start)
-    Router.events.on('routeChangeComplete', end)
-    Router.events.on('routeChangeError', end)
-    return () => {
-      Router.events.off('routeChangeStart', start)
-      Router.events.off('routeChangeComplete', end)
-      Router.events.off('routeChangeError', end)
-    }
-  }, [])
   const Layout = Component.Layout ?? Noop
 
-  return loading ? (
-    <h1>Loading...</h1>
-  ) : (
+  return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
