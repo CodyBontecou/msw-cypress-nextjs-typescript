d50001150b8e85f2818fb17de87662e318caa8e5
Cody Bontecou <bontecouc@gmail.com>
2021-08-30 17:26:33 -0500

    window object within mswjs index.js

--- mocks/index.js
+++ mocks/index.js
@@ -1,7 +1,13 @@
+import { rest } from 'msw'
+
 if (typeof window === 'undefined') {
   const { server } = require('./server')
   server.listen()
 } else {
   const { worker } = require('./browser')
   worker.start()
+  window.msw = {
+    worker,
+    rest,
+  }
 }
