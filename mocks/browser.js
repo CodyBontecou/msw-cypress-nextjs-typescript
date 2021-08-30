3bcaf61b2c0d036472d6e6191c801f1ec6668a45
Cody Bontecou <bontecouc@gmail.com>
2021-08-30 17:35:51 -0500

    exporting to window object through browser.js

--- mocks/browser.js
+++ mocks/browser.js
@@ -1,20 +1,5 @@
 import { setupWorker, rest } from 'msw'
 
-const url = 'https://jsonplaceholder.typicode.com/posts'
+export const worker = setupWorker(...handlers)
 
-export const worker = setupWorker(
-  rest.get(url, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          userId: 1,
-          id: 1,
-          title:
-            'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-          body: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
-        },
-      ])
-    )
-  })
-)
+window.msw = { worker, rest }
