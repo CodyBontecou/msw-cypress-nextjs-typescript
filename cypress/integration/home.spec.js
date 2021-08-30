a9a8d67cd2da7736ceadd1c84eb3ff740c9847a2
Cody Bontecou <bontecouc@gmail.com>
2021-08-30 17:27:00 -0500

    window.msw logic within tests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,27 +2,6 @@ import { rest, setupWorker } from 'msw'
 
 const url = 'https://jsonplaceholder.typicode.com/posts'
 
-const worker = setupWorker(
-  rest.get(url, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          userId: 1,
-          id: 1,
-          title:
-            'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-          body: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
-        },
-      ])
-    )
-  })
-)
-
-beforeEach(() => worker.start())
-afterEach(() => worker.resetHandlers())
-after(() => worker.stop())
-
 describe('Homepage tests', () => {
   it('loads expected data', () => {
     cy.visit('/')
@@ -36,23 +15,36 @@ describe('Homepage tests', () => {
     cy.visit('/about')
     cy.get('.rounded-full').click()
     cy.get('[data-test="dropdown-home"]').click()
-    worker.use(rest.get(url, (req, res, ctx) => res(ctx.delay('infinite'))))
-    cy.wait(500)
+
+    cy.window().then(window => {
+      // Reference global instances set in "src/mocks.js".
+      const { worker, rest } = window.msw
+
+      worker.use(rest.get(url, (req, res, ctx) => res(ctx.delay('infinite'))))
+    })
     cy.get('#nprogress').should('exist')
   })
 
-  it('shows when error occurs', () => {
+  it.only('shows when error occurs', () => {
     cy.visit('/')
-    worker.use(
-      rest.get(url, (req, res, ctx) =>
-        res(
-          ctx.status(404),
-          ctx.json({
-            errorMessage: 'Something weird happened',
-          })
+    cy.window().then(window => {
+      const { worker, rest } = window.msw
+      // beforeEach(() => worker.start())
+      // afterEach(() => worker.resetHandlers())
+      // after(() => worker.stop())
+
+      worker.use(
+        rest.get(url, (req, res, ctx) =>
+          res(
+            ctx.status(404),
+            ctx.json({
+              errorMessage: 'Something weird happened',
+            })
+          )
         )
       )
-    )
+    })
+
     cy.get('h1').should('contain.text', 'Error')
   })
 })
